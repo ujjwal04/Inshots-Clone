9f04b14309b823a5f3c67d2c45e23601078440ea
ujjwal <ujjawalverma77@gmail.com>
2020-07-05 18:23:35 +0530

    Updates

---
+++ precache-manifest.d0197f43c064476942f5fc3a27f494c0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba39124b8d77447460fb67d5135f9d91",
+    "url": "/Inshots-clone/index.html"
+  },
+  {
+    "revision": "7615649a75d553c7299d",
+    "url": "/Inshots-clone/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "091907b1ba707539b4f3",
+    "url": "/Inshots-clone/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7615649a75d553c7299d",
+    "url": "/Inshots-clone/static/js/2.90a94283.chunk.js"
+  },
+  {
+    "revision": "544111c69119227bbf075a65771ccdb1",
+    "url": "/Inshots-clone/static/js/2.90a94283.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "091907b1ba707539b4f3",
+    "url": "/Inshots-clone/static/js/main.5a4f28d7.chunk.js"
+  },
+  {
+    "revision": "4691953d632425d99101",
+    "url": "/Inshots-clone/static/js/runtime-main.372bdd1a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Inshots-clone/static/media/logo.5d5d9eef.svg"
+  }
+]);
